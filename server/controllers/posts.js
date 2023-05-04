a23b76c270f1ab1c02435a07229a5dc7a6d15d32
rholibobo <rholipop@gmail.com>
2023-05-04 19:18:06 +0100

    Corrected picturePath

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -12,7 +12,7 @@ export const createPost = async (req, res) => {
       lastName: user.lastName,
       location: user.location,
       description,
-      userPicturePath: user.picurePath,
+      userPicturePath: user.picturePath,
       picturePath,
       likes: {},
       comments: [],
